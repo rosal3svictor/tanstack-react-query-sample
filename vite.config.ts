049019d30321d58e94ccb93b6de22526e1aaee5f
Victor Rosales <rosalesvictor.dev@gmail.com>
2023-06-10 09:27:34 -0500

    chore: update development dependencies to use @vitejs/plugin-react-swc
    it was verified that that build process is running as expected and also it was updated some of the
    dependencies to keep them up-to-date

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
